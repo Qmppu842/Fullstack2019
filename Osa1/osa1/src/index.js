73afa6fb5b9871699eca127058c29c848be556ae
Qmppu842 <omppu842@gmail.com>
2019-01-21 21:15:55 +0200

    Added footer

--- Osa1/osa1/src/index.js
+++ Osa1/osa1/src/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () =>{
+const Hello = (props) => {
     return (
         <div>
-            <p>Hellou!</p>
+            <p>Hellou {props.name}, you are {props.age} years old!</p>
         </div>
 
     )
 }
+const Footer = () => {
+    return (
+        <div>
+            Gretting app created by <a href="https://github.com/Qmppu842">Qmppu842</a>
+        </div>
+    )
+}
 
 const App = () => {
-    return(
+    const nimi = 'Pekka'
+    const ika = 10
+
+    return (
         <div>
             <h1>Greetings earthlings!</h1>
-            <Hello />
-            <Hello />
-            <Hello />
-            
+            <Hello name="Arto" age={26 + 10} />
+            <Hello name={nimi} age={ika} />
+            <Footer />
         </div>
     )
 }
