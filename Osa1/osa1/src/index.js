a925a2919b257b071408d2e588478f37349eb00f
Qmppu842 <omppu842@gmail.com>
2019-01-28 01:14:37 +0200

    yee part one done....

--- Osa1/osa1/src/index.js
+++ Osa1/osa1/src/index.js
@@ -1,35 +1,56 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const History = ({ allClicks }) => {
+    if (allClicks.length === 0) {
+        return (
+            <div>
+                sovellusta käytetään nappeja painelemalla
+            </div>
+        )
+    }
 
+    return (
+        <div>
+            näppäilyhistoria: {allClicks.join(' ')}
+        </div>
+    )
+}
 
+const Button = ({ handleClick, text }) => {
+    console.log(handleClick)
+    return(
+    <button onClick={handleClick}>
+        {text}
+    </button>
+    )
+}
 
 const App = (props) => {
-    const [clicks, setClicks] = useState({
-        left: 0, right: 0
-    })
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
 
     const handleLeftClick = () => {
-        setClicks({ ...clicks, left: clicks.left + 1 })
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
     }
 
     const handleRightClick = () => {
-        setClicks({ ...clicks, left: clicks.right + 1 })
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
     }
 
 
     return (
         <div>
             <div>
-                {clicks.left}
-                <button onClick={handleLeftClick}>
-                    vasen
-                </button>
-                <button onClick={handleRightClick}>
-                    oikea
-                </button>
-                {clicks.right}
+                {left}
+                <Button handleClick={handleLeftClick} text='vasen' />
+                <Button handleClick={handleRightClick} text='oikea' />
+                {right}
+                <History allClicks={allClicks} />
             </div>
         </div>
     )
