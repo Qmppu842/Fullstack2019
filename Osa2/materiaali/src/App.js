44cc03344a80c30282550de03adca79096f0e6f2
Qmppu842 <omppu842@gmail.com>
2019-01-28 17:50:05 +0200

    Pre tehtavat commit

--- Osa2/materiaali/src/App.js
+++ Osa2/materiaali/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ notes }) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState('uusinta uutuuuttaaa.')
+    const [showAll, setShowAll] = useState(true)
+
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
 
     const rows = () => {
         return (
-            notes.map(note =>
+            notesToShow.map(note =>
                 <Note
                     key={note.id}
                     note={note}
@@ -14,12 +21,45 @@ const App = ({ notes }) => {
         )
     }
 
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log("Kaapauuuus!!!", event.target)
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1,
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+
+    }
+
+
     return (
         <div>
             <h1>Muistiinpanot</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    näytä {showAll ? 'vain tärkeät' : 'kaikki'}
+                </button>
+            </div>
             <ul>
                 {rows()}
             </ul>
+
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">tallenna</button>
+            </form>
         </div>
     )
 }
