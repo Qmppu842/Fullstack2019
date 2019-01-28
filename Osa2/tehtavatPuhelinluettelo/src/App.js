45d1a0895aeb77038cbf4c35d5e8831867c574f6
Qmppu842 <omppu842@gmail.com>
2019-01-28 19:16:53 +0200

    2.9:puhelinluettelo step4

--- Osa2/tehtavatPuhelinluettelo/src/App.js
+++ Osa2/tehtavatPuhelinluettelo/src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', 
-        number: '050-12345678' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [fog, setFog] = useState('')
+
+const notInFog = persons.filter(person => person.name.toLowerCase().includes(fog) || person.number.includes(fog))
 
     const rows = () => {
         return (
-            persons.map(person =>
+            notInFog.map(person =>
                 <p key={person.name}>{person.name} {person.number}</p>)
         )
     }
@@ -23,6 +28,10 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleFogInputChange = (event) => {
+        setFog(event.target.value)
+        console.log("Nooouu not the FOG! Anything but", fog)
+    }
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -48,7 +57,11 @@ const App = () => {
     return (
         <div>
             <h2>Puhelinluettelo</h2>
-            <div> Debug: {newName}</div>
+            <div>
+                rajaa näytettävää aluetta: <input
+                    onChange={handleFogInputChange} />
+            </div>
+            <h3>Lisää uusi</h3>
             <form>
                 <div>
                     nimi:
