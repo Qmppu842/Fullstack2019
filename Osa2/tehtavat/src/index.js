fe98bd34412c2f61632341bc1b5b969f976e10ad
Qmppu842 <omppu842@gmail.com>
2019-01-28 13:28:09 +0200

    Base for part2 material and exercises

---
+++ Osa2/tehtavat/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Header = (head) => {
+    return (
+        <>
+            <h1>{head.course}</h1>
+        </>
+    )
+}
+const Part = (part) => {
+    return (
+        <>
+            <p>
+                {part.part.name} {part.part.exercises}
+            </p>
+        </>
+    )
+}
+const Content = (props) => {
+    return (
+        <>
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
+        </>
+    )
+}
+
+
+const Total = (totals) => {
+    return (
+        <>
+            <p>yhteensä {totals.total[0].exercises + totals.total[1].exercises + totals.total[2].exercises} kurssia</p>
+        </>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+            {
+                name: 'Reactin perusteet',
+                exercises: 10
+            },
+            {
+                name: 'Tiedonvälitys propseilla',
+                exercises: 7
+            },
+            {
+                name: 'Komponenttien tilaa',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total total={course.parts} />
+        </div>
+
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
